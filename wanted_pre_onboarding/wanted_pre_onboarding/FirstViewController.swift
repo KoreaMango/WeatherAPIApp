7dcf895fad1fb7bd21ca72ee8e14c29c5da085f4
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-08-31 17:07:05 +0900

    tableView setting

--- wanted_pre_onboarding/wanted_pre_onboarding/FirstViewController.swift
+++ wanted_pre_onboarding/wanted_pre_onboarding/FirstViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, UITableViewDataSource {
+    //MARK: - Properties
+    @IBOutlet weak var tableView : UITableView!
+    let cellIdentifier: String = "Cell"
+    
+    //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    //MARK: - TableView
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        
+        return cell
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        
+        
+    }
+    
 }
 
