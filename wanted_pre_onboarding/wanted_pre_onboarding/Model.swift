33a5da2807f22730e3167341cadfd805fae05150
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-08-31 16:52:07 +0900

    make File

---
+++ wanted_pre_onboarding/wanted_pre_onboarding/Model.swift
@@ -0,0 +1,17 @@
+//
+//  Model.swift
+//  wanted_pre_onboarding
+//
+//  Created by 강민규 on 2022/08/31.
+//
+
+import Foundation
+import UIKit
+
+struct Weather {
+    let country : String
+    let weatherIcon : UIImage
+    let temperature : Int
+    let humidity : Int
+    
+}
