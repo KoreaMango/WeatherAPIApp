3f92476211f0809e532a626f7cbbe96497dbdea5
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-08-31 18:03:56 +0900

    API interface setting

--- wanted_pre_onboarding/wanted_pre_onboarding/Service/API.swift
+++ wanted_pre_onboarding/wanted_pre_onboarding/Service/API.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import UIKit
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+private let apiKey = "7d44cd3f14e73a7ec226c506bb668083"
+private let baseURL = "api.openweathermap.org"
+
+class API {
+    public static var shared = API()
+    private var weathers : [Weather] = []
+    
+    func requestURL() {
+        
+    }
+    
+    func getData(){
+        
+    }
+    
+    func getCountry(index : Int) -> String {
+        
+        return ""
+    }
+    
+    func getWeatherIcon(index: Int) -> UIImage {
+        
+        return UIImage()
+    }
+    func getTemperature(index: Int) -> Int {
+        
+        return 0
+    }
+    func getHumidity(index: Int) -> Int {
+        
+        return 0
+    }
+}
