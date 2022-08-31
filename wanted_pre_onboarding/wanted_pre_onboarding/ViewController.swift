a352a7ae2ef99cad9bae234cb9262a5a2991f6c9
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-08-31 15:05:26 +0900

    project init

---
+++ wanted_pre_onboarding/wanted_pre_onboarding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wanted_pre_onboarding
+//
+//  Created by 강민규 on 2022/08/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
