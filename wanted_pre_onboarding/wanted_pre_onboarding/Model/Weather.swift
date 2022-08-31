68b359a567396edad352b277037f7a33c20f534c
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-08-31 20:08:48 +0900

    Model add

--- wanted_pre_onboarding/wanted_pre_onboarding/Model/Weather.swift
+++ wanted_pre_onboarding/wanted_pre_onboarding/Model/Weather.swift
@@ -5,13 +5,68 @@
 //  Created by 강민규 on 2022/08/31.
 //
 
+/*
+{"coord":
+        {"lon":126.9156,"lat":35.1547},
+"weather":
+        [{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04d"}],
+"base":
+        "stations",
+"main":
+     {   "temp":296.8,
+         "feels_like":297.26,
+         "temp_min":296.8,
+         "temp_max":296.8,
+         "pressure":1007,
+         "humidity":78,
+         "sea_level":1007,
+         "grnd_level":1002
+     },
+"visibility":
+        10000,
+"wind":
+        {"speed":2.94,"deg":332,"gust":5.08},
+"clouds":
+        {"all":97},
+"dt":
+        1661937465,
+"sys":
+    {    "country":"KR",
+         "sunrise":1661893435,
+         "sunset":1661940131
+     },
+"timezone":
+        32400,
+"id":
+        1841811,
+"name":
+        "Gwangju",
+"cod":
+        200
+ }
+ 
+ 
+ */
+
 import Foundation
 import UIKit
 
-struct Weather {
-    let country : String
-    let weatherIcon : UIImage
-    let temperature : Int
-    let humidity : Int
-    
+struct Data : Codable{
+    var weather : [Weather]
+    var main : Main
+    var name : String
+}
+
+struct Main : Codable {
+    var temp : Double
+    var humidity : Int
+    var feels_like : Double
+    var temp_min : Double
+    var temp_max : Double
+    var pressure : Int
+}
+
+struct Weather : Codable {
+    var main : String
+    var icon : String
 }
