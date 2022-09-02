25e606b2c06248bf964990214687cbf7141d76bd
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-09-03 00:31:05 +0900

    secondView Var add

--- wanted_pre_onboarding/wanted_pre_onboarding/ViewController/SecondViewController.swift
+++ wanted_pre_onboarding/wanted_pre_onboarding/ViewController/SecondViewController.swift
@@ -8,6 +8,17 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    @IBOutlet weak var imageViewIcon : UIImageView?
+    @IBOutlet weak var labelName : UILabel?
+    @IBOutlet weak var labelMain: UILabel?
+    @IBOutlet weak var labelTemp: UILabel?
+    @IBOutlet weak var labelMinTemp: UILabel?
+    @IBOutlet weak var labelMaxTemp: UILabel?
+    @IBOutlet weak var labelHumidity: UILabel?
+    @IBOutlet weak var labelPressure : UILabel?
+    @IBOutlet weak var labelFeelLike : UILabel?
+    @IBOutlet weak var labelWind : UILabel?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
